9668891c3b44b08be31c9132ce3a40ac5688b35d
David Todd <me@david.to>
2020-05-22 13:47:22 +0100

    Added string type check before procressing

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-'use-strict';
+'use-strict'
 
 function radioButtonRedirect(req, res, next) {
-  const obj = Object.keys(req.body).length ? req.body : req.query;
-  for (const k in obj) {
-    const v = obj[k];
-    if (v.includes('~')) {
-      const parts = v.split('~');
-      req.session.data[k] = parts[0];
-      const href = parts[1];
-      return res.redirect(href);
-    }
-  }
-  next();
+	const obj = Object.keys(req.body).length ? req.body : req.query
+	for (const k in obj) {
+		const v = obj[k]
+		if (typeof v === 'string' || v instanceof String) {
+			if (v.includes('~')) {
+				const parts = v.split('~')
+				req.session.data[k] = parts[0]
+				const href = parts[1]
+				return res.redirect(href)
+			}
+		}
+	}
+	next()
 }
 
 module.exports = radioButtonRedirect
